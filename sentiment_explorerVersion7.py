a010fd8f1ff24c3bb1c355536aeec2811113f176
Glen Low <glen.low.2021@scis.smu.edu.sg>
2023-06-28 14:40:39 +0800

    Testing 4

--- sentiment_explorerVersion7.py
+++ sentiment_explorerVersion7.py
@@ -176,8 +176,8 @@ def newtext_fullstop(text):
 
     text = text.replace("n't"," not")
     text = text.replace("n't"," not")
-    text = text.replace("don "," not")
-    text = text.replace("dun "," not")
+    text = text.replace("don"," not")
+    text = text.replace("dun"," not")
     text = text.replace("'s"," is")
     text = text.replace("'s"," is")
     text = text.replace("'ve"," have")
