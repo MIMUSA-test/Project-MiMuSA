f9aae7c3df8065a0f0400b6a730ccdff45b38cf9
Seah Yok Sim <yoksim.seah.2021@scis.smu.edu.sg>
2023-11-13 23:57:55 +0800

    Update Config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// const model1 = "http://127.0.0.1:8000/generate"
-// const model2 = "http://127.0.0.1:8000/generate2"
+// const model1 = "http://127.0.0.1:8080/generate"
+// const model2 = "http://127.0.0.1:8080/generate2"
 const model1 = "https://model.socialopinionanalytics.net/generate"
 const model2 = "https://model.socialopinionanalytics.net/generate2"
 
