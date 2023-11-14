82ef54c2de52d8680be69c625612ac541c4722d4
Seah Yok Sim <yoksim.seah.2021@scis.smu.edu.sg>
2023-11-14 14:48:11 +0800

    Remove config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// const model1 = "http://127.0.0.1:8080/generate"
-// const model2 = "http://127.0.0.1:8080/generate2"
-const model1 = "https://model.socialopinionanalytics.net/generate"
-const model2 = "https://model.socialopinionanalytics.net/generate2"
-
-export {model1, model2};
